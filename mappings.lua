4c59c3fdd276779ba769ee80c25ca48d61ba0c70
Seth <src01@pm.me>
2023-06-12 08:28:18 -0400

    first commit

---
+++ mappings.lua
@@ -0,0 +1,40 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>" },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
+
+M.crates = {
+  plugin = true,
+  n = {
+    ["<leader>rcu"] = {
+      function ()
+        require('crates').upgrade_all_crates()
+      end,
+      "update crates"
+    }
+  }
+}
+
+M.Lazygit = {
+  n = {
+    ["<leader>gg"] = {
+      "<cmd>LazyGit<CR>",
+      "LazyGit",
+    },
+  },
+}
+
+
+return M
